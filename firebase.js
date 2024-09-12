5a7b62071d5037c70c27ae9d470ca43368def01a
VOTHIPHUONG <162528256+VOTHIPHUONG@users.noreply.github.com>
2024-09-13 05:19:29 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,22 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set } from 'firebase/database';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js';
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js';
+import { getFirestore, doc, setDoc } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_i9NWCqb04Imfw4UW0BqLE4jvzOi0t-M",
-  authDomain: "gamejrf-4423b.firebaseapp.com",
-  projectId: "gamejrf-4423b",
-  storageBucket: "gamejrf-4423b.appspot.com",
-  messagingSenderId: "915828708458",
-  appId: "1:915828708458:web:9b4f5350ae2f8c41f5a613",
-  measurementId: "G-XJV3ZFT98F",
-  databaseURL: "https://gamejrf-4423b-default-rtdb.firebaseio.com/" // Cần có URL của Realtime Database
+    apiKey: "AIzaSyB_i9NWCqb04Imfw4UW0BqLE4jvzOi0t-M",
+    authDomain: "gamejrf-4423b.firebaseapp.com",
+    projectId: "gamejrf-4423b",
+    storageBucket: "gamejrf-4423b.appspot.com",
+    messagingSenderId: "915828708458",
+    appId: "1:915828708458:web:9b4f5350ae2f8c41f5a613",
+    measurementId: "G-XJV3ZFT98F"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const db = getFirestore(app);
+const realtimeDb = getDatabase(app);
+
+// Your game logic here
